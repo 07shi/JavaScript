4cad94d876eaded306dbecea80e46d53740bde41
Shivam Singh <147870844+07shi@users.noreply.github.com>
2026-03-15 10:09:21 +0000

    datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,20 @@
+"use strict";// treat all js code as newer version
+
+// alert(3+3) // we are using nodjs, not browser
+
+console.log(3+3);//console.log("shivam") // code readability is not good
+console.log("shivam");// code readability should be high in these case
+
+let name = "shivma"
+let age = 18
+let isLoggedIn = false
+
+//number => 2 to power 53
+// bigInt
+// string => ""
+//boolean
+//null => standalone value
+// undefined =>
+// symbol
+console.log(typeof "hitesh");
+console.log(typeof null); // object
